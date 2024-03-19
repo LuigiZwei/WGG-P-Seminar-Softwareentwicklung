0368b9086b78295423165c94d8bfc1ec6cce6936
Maxim <maxim.klinch29@gmail.com>
2024-03-19 14:53:28 +0100

    player position is changed when the room is changed

---
+++ scripts/main.gd
@@ -0,0 +1,60 @@
+extends Node2D
+
+var main_scene
+var main_instance
+var player_scene
+var player_instance
+var status
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	status = "main menu"
+	var main_menu_button_container = get_node("CanvasLayer/Control/VBoxContainer/HBoxContainer2/PanelContainer/MarginContainer/VBoxContainer")
+	var settings_button_container = get_node("CanvasLayer/Control2/PanelContainer/MarginContainer/HBoxContainer")
+	var pause_button_container = get_node("CanvasLayer/Control3/PanelContainer/MarginContainer/VBoxContainer")
+	
+	main_menu_button_container.get_node("Button").connect("pressed", start_game)
+	main_menu_button_container.get_node("Button2").connect("pressed", get_node("CanvasLayer/Control2").show)
+	main_menu_button_container.get_node("Button2").connect("pressed", get_node("CanvasLayer/Control").hide)
+	main_menu_button_container.get_node("Button3").connect("pressed", get_tree().quit)
+	
+	settings_button_container.get_node("Button").connect("pressed", get_node("CanvasLayer/Control2").hide)
+	settings_button_container.get_node("Button").connect("pressed", get_node("CanvasLayer/Control").show)
+	
+	pause_button_container.get_node("Button").connect("pressed", get_node("CanvasLayer/Control3").hide)
+	pause_button_container.get_node("Button2").connect("pressed", close_game)
+	pause_button_container.get_node("Button3").connect("pressed", get_tree().quit)
+
+
+func start_game():
+	get_node("CanvasLayer/Control").hide()
+	status = "game"
+	
+	get_node("chemie-saal").show()
+	get_node("player").show()
+	
+	var chemistry_door_node = get_node("/root/main/player")
+	chemistry_door_node.connect("switch_room", switch_room, 0)
+
+func close_game():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	if Input.is_action_just_pressed("pause") && (status == "game"):
+		if get_node("CanvasLayer/Control3").visible:
+			get_node("CanvasLayer/Control3").hide()
+		else:
+			get_node("CanvasLayer/Control3").show()
+	
+	
+
+func switch_room(new_room, playerPosition): #switches the current main_room with another new new_room
+	get_node("chemie-saal").queue_free() 
+	main_scene = load(new_room) 
+	main_instance = main_scene.instantiate()  
+	add_child(main_instance)
+	get_node("player").position = playerPosition
+	
+
---
+++ scripts/player.gd
@@ -0,0 +1,41 @@
+extends CharacterBody2D
+
+const player_speed = 300
+const player_sprint_speed = 600
+var speed
+signal switch_room(path,player_position)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	if(is_visible()):
+		if Input.is_action_pressed("sprint"):
+			speed = player_sprint_speed
+		else:
+			speed = player_speed
+		if Input.is_action_pressed("ui_left"):
+			velocity += Vector2.LEFT
+		if Input.is_action_pressed("ui_right"):
+			velocity += Vector2.RIGHT
+		if Input.is_action_pressed("ui_up"):
+			velocity += Vector2.UP
+		if Input.is_action_pressed("ui_down"):
+			velocity += Vector2.DOWN
+	
+		velocity = velocity.normalized() * speed
+		move_and_slide()
+		velocity=Vector2(0,0)
+	
+		if(get_last_slide_collision()!=null):
+			var collider = get_last_slide_collision().get_collider()
+			if(collider.has_meta("path") ):
+				switch_room.emit(collider.get_meta("path"),collider.get_meta("player_position"))
+		pass
+	
+
+#called every frame, used for physics stuff
+func _physics_process(_delta):
+	pass
