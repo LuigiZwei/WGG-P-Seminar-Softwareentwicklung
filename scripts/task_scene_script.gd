7c334047ba1dc9b8efd768dd8c5ad5cb9cbf73d9
pngfunqbjqbg <153130816+pngfunqbjqbg@users.noreply.github.com>
2024-04-18 16:23:30 +0200

    Add files via upload

--- scripts/task_scene_script.gd
+++ scripts/task_scene_script.gd
@@ -1,23 +1,39 @@
 extends Control
 
+var solution
+var task_text
+var task_input
+var task_output
+# task_output ist die LineEdit node, die den input des spielers beinhält
+# ich habe sie "output" genannt weil der name "input" für den input, den der spieler kriegt, besser passt
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var vboxcontainer = $PanelContainer/MarginContainer/VBoxContainer/ScrollContainer/VBoxContainer
+	task_text = vboxcontainer.get_node("Label")
+	task_input = vboxcontainer.get_node("TextEdit")
+	task_output = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/LineEdit
+	
+	solution = "42"
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
 
-
 func switch_task_and_input():
-	var text_vboxcontainer = $PanelContainer/MarginContainer/VBoxContainer/ScrollContainer/VBoxContainer
-	if !text_vboxcontainer.get_node("Label").visible:
-		text_vboxcontainer.get_node("Label").show()
-		text_vboxcontainer.get_node("TextEdit").hide()
+	# wechselt sichtbarkeit zwischen dem Aufgabentext und dem Input-text
+	# die größe des jeweiligen textfeldes wird automatisch durch control-nodes bestimmt
+	if !task_text.visible:
+		task_text.show()
+		task_input.hide()
 		$PanelContainer/MarginContainer/VBoxContainer/Button.text = "Input zeigen"
 	else:
-		text_vboxcontainer.get_node("Label").hide()
-		text_vboxcontainer.get_node("TextEdit").show()
+		task_text.hide()
+		task_input.show()
 		$PanelContainer/MarginContainer/VBoxContainer/Button.text = "Aufgabenstellung zeigen"
+
+func check_for_solution(_new_text):
+	if(task_output.text == solution):
+		# richtige antwort
+		if(task_output.has_focus()):
+			task_output.release_focus()
+	else:
+		# falsche antwort
